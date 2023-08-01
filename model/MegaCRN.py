429d117bd3999c477c847562dbc1b027c076881c
Renhe Jiang <jiangrh@csis.u-tokyo.ac.jp>
2023-08-02 00:06:48 +0900

    update MegaCRN.py

--- model/MegaCRN.py
+++ model/MegaCRN.py
@@ -51,24 +51,24 @@ class AGCRNCell(nn.Module):
         return torch.zeros(batch_size, self.node_num, self.hidden_dim)
     
 class ADCRNN_Encoder(nn.Module):
-    def __init__(self, node_num, dim_in, dim_out, cheb_k, num_layers):
+    def __init__(self, node_num, dim_in, dim_out, cheb_k, rnn_layers):
         super(ADCRNN_Encoder, self).__init__()
-        assert num_layers >= 1, 'At least one DCRNN layer in the Encoder.'
+        assert rnn_layers >= 1, 'At least one DCRNN layer in the Encoder.'
         self.node_num = node_num
         self.input_dim = dim_in
-        self.num_layers = num_layers
+        self.rnn_layers = rnn_layers
         self.dcrnn_cells = nn.ModuleList()
         self.dcrnn_cells.append(AGCRNCell(node_num, dim_in, dim_out, cheb_k))
-        for _ in range(1, num_layers):
+        for _ in range(1, rnn_layers):
             self.dcrnn_cells.append(AGCRNCell(node_num, dim_out, dim_out, cheb_k))
 
     def forward(self, x, init_state, supports):
-        #shape of x: (B, T, N, D), shape of init_state: (num_layers, B, N, hidden_dim)
+        #shape of x: (B, T, N, D), shape of init_state: (rnn_layers, B, N, hidden_dim)
         assert x.shape[2] == self.node_num and x.shape[3] == self.input_dim
         seq_length = x.shape[1]
         current_inputs = x
         output_hidden = []
-        for i in range(self.num_layers):
+        for i in range(self.rnn_layers):
             state = init_state[i]
             inner_states = []
             for t in range(seq_length):
@@ -78,35 +78,35 @@ class ADCRNN_Encoder(nn.Module):
             current_inputs = torch.stack(inner_states, dim=1)
         #current_inputs: the outputs of last layer: (B, T, N, hidden_dim)
         #last_state: (B, N, hidden_dim)
-        #output_hidden: the last state for each layer: (num_layers, B, N, hidden_dim)
+        #output_hidden: the last state for each layer: (rnn_layers, B, N, hidden_dim)
         #return current_inputs, torch.stack(output_hidden, dim=0)
         return current_inputs, output_hidden
     
     def init_hidden(self, batch_size):
         init_states = []
-        for i in range(self.num_layers):
+        for i in range(self.rnn_layers):
             init_states.append(self.dcrnn_cells[i].init_hidden_state(batch_size))
         return init_states
 
 class ADCRNN_Decoder(nn.Module):
-    def __init__(self, node_num, dim_in, dim_out, cheb_k, num_layers):
+    def __init__(self, node_num, dim_in, dim_out, cheb_k, rnn_layers):
         super(ADCRNN_Decoder, self).__init__()
-        assert num_layers >= 1, 'At least one DCRNN layer in the Decoder.'
+        assert rnn_layers >= 1, 'At least one DCRNN layer in the Decoder.'
         self.node_num = node_num
         self.input_dim = dim_in
-        self.num_layers = num_layers
+        self.rnn_layers = rnn_layers
         self.dcrnn_cells = nn.ModuleList()
         self.dcrnn_cells.append(AGCRNCell(node_num, dim_in, dim_out, cheb_k))
-        for _ in range(1, num_layers):
+        for _ in range(1, rnn_layers):
             self.dcrnn_cells.append(AGCRNCell(node_num, dim_out, dim_out, cheb_k))
 
     def forward(self, xt, init_state, supports):
         # xt: (B, N, D)
-        # init_state: (num_layers, B, N, hidden_dim)
+        # init_state: (rnn_layers, B, N, hidden_dim)
         assert xt.shape[1] == self.node_num and xt.shape[2] == self.input_dim
         current_inputs = xt
         output_hidden = []
-        for i in range(self.num_layers):
+        for i in range(self.rnn_layers):
             state = self.dcrnn_cells[i](current_inputs, init_state[i], supports)
             output_hidden.append(state)
             current_inputs = state
@@ -114,7 +114,7 @@ class ADCRNN_Decoder(nn.Module):
 
 
 class MegaCRN(nn.Module):
-    def __init__(self, num_nodes, input_dim, output_dim, horizon, rnn_units, num_layers=1, cheb_k=3,
+    def __init__(self, num_nodes, input_dim, output_dim, horizon, rnn_units, rnn_layers=1, cheb_k=3,
                  ycov_dim=1, mem_num=20, mem_dim=64, cl_decay_steps=2000, use_curriculum_learning=True):
         super(MegaCRN, self).__init__()
         self.num_nodes = num_nodes
@@ -122,7 +122,7 @@ class MegaCRN(nn.Module):
         self.rnn_units = rnn_units
         self.output_dim = output_dim
         self.horizon = horizon
-        self.num_layers = num_layers
+        self.rnn_layers = rnn_layers
         self.cheb_k = cheb_k
         self.ycov_dim = ycov_dim
         self.cl_decay_steps = cl_decay_steps
@@ -134,11 +134,11 @@ class MegaCRN(nn.Module):
         self.memory = self.construct_memory()
 
         # encoder
-        self.encoder = ADCRNN_Encoder(self.num_nodes, self.input_dim, self.rnn_units, self.cheb_k, self.num_layers)
+        self.encoder = ADCRNN_Encoder(self.num_nodes, self.input_dim, self.rnn_units, self.cheb_k, self.rnn_layers)
         
         # deocoder
         self.decoder_dim = self.rnn_units + self.mem_dim
-        self.decoder = ADCRNN_Decoder(self.num_nodes, self.output_dim + self.ycov_dim, self.decoder_dim, self.cheb_k, self.num_layers)
+        self.decoder = ADCRNN_Decoder(self.num_nodes, self.output_dim + self.ycov_dim, self.decoder_dim, self.cheb_k, self.rnn_layers)
 
         # output
         self.proj = nn.Sequential(nn.Linear(self.decoder_dim, self.output_dim, bias=True))
@@ -178,7 +178,7 @@ class MegaCRN(nn.Module):
         h_att, query, pos, neg = self.query_memory(h_t)
         h_t = torch.cat([h_t, h_att], dim=-1)
         
-        ht_list = [h_t]*self.num_layers
+        ht_list = [h_t]*self.rnn_layers
         go = torch.zeros((x.shape[0], self.num_nodes, self.output_dim), device=x.device)
         out = []
         for t in range(self.horizon):
