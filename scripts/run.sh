ac31c06b78ce4da0de6475edefd66d3b88f39682
kevin-xuan <raoxuanzzz@gmail.com>
2023-08-02 20:06:41 +0900

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,19 +1,19 @@
 cd model
 
 #* fine-tune the lamb hyperparameter
-python traintest_MemGCRN.py --lamb 0.01 --temp 0.1   
+# python traintest_MemGCRN.py --lamb 0.01 --temp 0.1   
 python traintest_MemGCRN.py --lamb 0.1 --temp 0.1
 python traintest_MemGCRN.py --lamb 1 --temp 0.1
 python traintest_MemGCRN.py --lamb 10 --temp 0.1
 
-python traintest_MemGCRN.py --lamb 0.01 --temp 0.1 --contra_denominator
+# python traintest_MemGCRN.py --lamb 0.01 --temp 0.1 --contra_denominator
 python traintest_MemGCRN.py --lamb 0.1 --temp 0.1 --contra_denominator
 python traintest_MemGCRN.py --lamb 1 --temp 0.1 --contra_denominator
 python traintest_MemGCRN.py --lamb 10 --temp 0.1 --contra_denominator
 
-#* fine-tune the temperature hyperparameter
-# python traintest_MemGCRN.py --temp 0.1 --lamb 0.01   
-python traintest_MemGCRN.py --temp 0.5 --lamb 0.01
-python traintest_MemGCRN.py --temp 1 --lamb 0.01
+#* fine-tune the temperature hyperparameter  
+# python traintest_MemGCRN.py --temp 0.5 --lamb 0.01
+# python traintest_MemGCRN.py --temp 1 --lamb 0.01
+
 python traintest_MemGCRN.py --temp 0.5 --lamb 0.01 --contra_denominator
 python traintest_MemGCRN.py --temp 1 --lamb 0.01 --contra_denominator
