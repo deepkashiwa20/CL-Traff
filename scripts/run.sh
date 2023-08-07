924d25b4038eaf2a2c364bbeb15991e476b8b95f
kevin-xuan <raoxuanzzz@gmail.com>
2023-08-07 10:44:54 +0900

    fix Nonetype

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,9 @@ cd model
 
 #* fine-tune the lamb hyperparameter
 # python traintest_MemGCRN.py --lamb 0.01 --temp 0.1   
-python traintest_MemGCRN.py --lamb 0.1 --temp 0.1
-python traintest_MemGCRN.py --lamb 1 --temp 0.1
-python traintest_MemGCRN.py --lamb 10 --temp 0.1
+python traintest_MemGCRN.py --lamb 0.1 --temp 1.0
+python traintest_MemGCRN.py --lamb 1 --temp 1.0
+python traintest_MemGCRN.py --lamb 10 --temp 1.0 # worse
 
 # python traintest_MemGCRN.py --lamb 0.01 --temp 0.1 --contra_denominator
 python traintest_MemGCRN.py --lamb 0.1 --temp 0.1 --contra_denominator
