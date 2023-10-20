7fc4a05d9b204db5d1c403c340f8adc5609b3b40
Renhe Jiang <jiangrh@csis.u-tokyo.ac.jp>
2023-10-20 12:25:26 +0900

    using timeinday instead of weekdaytime

--- generate_training_data_plus.py
+++ generate_training_data_plus.py
@@ -5,12 +5,9 @@ from __future__ import unicode_literals
 
 import argparse
 import numpy as np
-import os, math
+import os
 import pandas as pd
-from collections import defaultdict
-WEEKDAYTIME = 7 * 24 * 12 - 1
-DAYTIME = 24 * 12
-HOURTIME = 12
+
 
 def generate_graph_seq2seq_io_data(data, x_offsets, y_offsets):
     """
@@ -39,8 +36,8 @@ def generate_graph_seq2seq_io_data(data, x_offsets, y_offsets):
 
 
 def generate_train_val_test(args):
-    df = pd.read_csv(args.data_file)
-    data = df[['speed', 'weekdaytime', 'speed_y']].values
+    df = pd.read_hdf(args.data_file)
+    data = df[['speed', 'timeinday', 'speed_y']].values
     data = data.reshape(-1, 207, 3)
     
     # 0 is the latest observed sample.
@@ -50,7 +47,7 @@ def generate_train_val_test(args):
     # x: (num_samples, input_length, num_nodes, input_dim)
     # y: (num_samples, output_length, num_nodes, output_dim)
     x, y = generate_graph_seq2seq_io_data(data, x_offsets=x_offsets, y_offsets=y_offsets)
-    
+
     print("x shape: ", x.shape, ", y shape: ", y.shape)
     # Write the data into npz file.
     # num_test = 6831, using the last 6831 examples as testing.
@@ -69,30 +66,13 @@ def generate_train_val_test(args):
     )
     # test
     x_test, y_test = x[-num_test:], y[-num_test:]
-    
-    #* construct time index
-    index_week = defaultdict(list)  # 10
-    index_day = defaultdict(list)  # 100
-    index_hour = defaultdict(list)  # 1000
-    for x in x_train:
-        x_speed = x[:, :, [0]].astype(np.float32)  # (T, N, 1)
-        current_time = x[0, 0, 1]  # ['speed', 'weekdaytime', 'speed_y']
-        weekdaytime = round(current_time * WEEKDAYTIME)  # 0~2015 
-        daytime = weekdaytime % DAYTIME
-        hourtime = weekdaytime % DAYTIME // HOURTIME
-        # print(current_time, current_time * WEEKDAYTIME, weekdaytime, daytime, hourtime)
-        index_week[weekdaytime].append(x_speed)
-        index_day[daytime].append(x_speed)
-        index_hour[hourtime].append(x_speed)
-    np.save(os.path.join(args.output_dir, "train_index_week.npy"), index_week)
-    np.save(os.path.join(args.output_dir, "train_index_day.npy"), index_day)
-    np.save(os.path.join(args.output_dir, "train_index_hour.npy"), index_hour)
-    
+
+
     for cat in ["train", "val", "test"]:
         _x, _y = locals()["x_" + cat], locals()["y_" + cat]
         print(cat, "x: ", _x.shape, "y:", _y.shape)
         np.savez_compressed(
-            os.path.join(args.output_dir, "%s.npz" % cat),
+            os.path.join(args.output_dir, "%splus.npz" % cat),
             x=_x,
             y=_y,
             x_offsets=x_offsets.reshape(list(x_offsets.shape) + [1]),
@@ -109,13 +89,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, choices=['METRLA', 'PEMSBAY'], default='METRLA', help='which dataset to run')
     parser.add_argument("--output_dir", type=str, default="METRLA/", help="Output directory.")
-    parser.add_argument("--data_file", type=str, default="METRLA/metr-la.h5", help="Raw traffic readings.")
+    parser.add_argument("--data_file", type=str, default="METRLA/metr-la-plus.h5", help="Raw traffic readings.")
     args = parser.parse_args()
     args.output_dir = f'{args.dataset}/'
     if args.dataset == 'METRLA':
-        # args.data_file = f'{args.dataset}/metr-la.h5'
-        args.data_file = f'{args.dataset}/metr-la.csv.gz'
+        args.data_file = f'{args.dataset}/metr-la-plus.h5'
     elif args.dataset == 'PEMSBAY':
-        # args.data_file = f'{args.dataset}/pems-bay.h5'
-        args.data_file = f'{args.dataset}/pems-bay.csv.gz'
+        args.data_file = f'{args.dataset}/pems-bay-plus.h5'
     main(args)
